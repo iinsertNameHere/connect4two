58063b9eac931c22ef8c89508a1cd19a03cc489b
iinsertNameHere <iinsert.mailhere@gmail.com>
2024-01-17 20:26:13 +0100

    Initial

---
+++ client.py
@@ -0,0 +1,210 @@
+import numpy as np
+import pygame
+import sys
+import math
+from requests import post, get
+import json
+
+server = input("Server URL: ").strip()
+
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
+ 
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+ 
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
+ 
+def is_valid_location(board, col):
+    return board[ROW_COUNT-1][col] == 0
+ 
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
+ 
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+ 
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+ 
+    # Check positively sloped diaganols
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+ 
+    # Check negatively sloped diaganols
+    for c in range(COLUMN_COUNT-3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+ 
+def draw_board(board):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+     
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):      
+            if board[r][c] == 1:
+                pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+            elif board[r][c] == 2: 
+                pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+    pygame.display.update()
+
+id = 0
+board = []
+game_over = False
+turn = 0
+winner = 0
+
+code = input("Join-Code: ").strip()
+
+def create_game():
+    global code, id
+
+    resp = get(server + "/create")
+    jresp = json.loads(resp.text)
+    id = jresp.get("playerid")
+    code = jresp.get("join_code")
+
+def join():
+    global id
+
+    resp = get(server + f"/{code}/join")
+    id = json.loads(resp.text).get('playerid')
+
+def sync():
+    global board, game_over, turn, winner
+
+    resp = get(server + f"/{code}/sync")
+    jresp = json.loads(resp.text)
+
+    board = np.asarray(jresp.get('board'))
+    game_over = jresp.get('gameover')
+    turn = jresp.get('turn')
+    winner = jresp.get('winner')
+
+def update(b):
+    post(server + f"/{code}/update", json={"playerid": id, "board": b.tolist()})
+
+def gameover(winner):
+    post(server + f"/{code}/gameover", json={"winner": winner})
+
+#initalize pygame
+pygame.init()
+ 
+#define our screen size
+SQUARESIZE = 100
+ 
+#define width and height of board
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
+ 
+size = (width, height)
+ 
+RADIUS = int(SQUARESIZE/2 - 5)
+ 
+screen = pygame.display.set_mode(size)
+pygame.display.update()
+ 
+myfont = pygame.font.SysFont("monospace", 75)
+
+while True:
+    id = 0
+    board = []
+    game_over = False
+    turn = 0
+    winner = 0
+    delay = 1000
+
+    if code == "":
+        create_game()
+        print("Your Join-Code:", code)
+    else:
+        join()
+    sync()
+
+    screen.fill(BLACK)
+    pygame.display.update()
+
+    label = myfont.render(f"Player {id}", 1, BLUE)
+    screen.blit(label, (40,10))
+    pygame.display.update()
+
+    while turn == 0:
+        sync()
+        pygame.time.wait(500)
+
+    pygame.time.wait(5000)
+
+    while not game_over:
+
+        if delay <= 0:
+            sync()
+            delay = 1000
+        delay -= 1
+
+        for event in pygame.event.get(): 
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+                posx = event.pos[0]
+                if turn != 0:
+                    if turn == id:
+                        if id == 1:
+                            pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+                        else:
+                            pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+                    else: 
+                        if id == 1:
+                            pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+                        else:
+                            pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+                
+
+            pygame.display.update()
+    
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.rect(screen, BLACK, (0,0, width, SQUARESIZE))
+                #print(event.pos)
+                # Ask for Player Input
+                if turn == id:
+                    localboard = board
+                    posx = event.pos[0]
+                    col = int(math.floor(posx/SQUARESIZE))
+    
+                    if is_valid_location(localboard, col):
+                        row = get_next_open_row(localboard, col)
+                        drop_piece(localboard, row, col, id)
+
+                        if winning_move(localboard, id):
+                            gameover(id)
+
+                        update(localboard)
+                    
+        draw_board(board)
+
+    if game_over:
+        if winner != 0:
+            label = myfont.render(f"Player {winner} wins!", 1, RED if winner == 1 else YELLOW)
+            screen.blit(label, (40,10))
+            pygame.display.update()
+        else:
+            label = myfont.render(f"No winner!", 1, BLUE)
+            screen.blit(label, (40,10))
+            pygame.display.update()
+    
+    pygame.time.wait(3000)
---
+++ server.py
@@ -0,0 +1,99 @@
+import numpy as np
+from fastapi import FastAPI, Request
+from random import randint
+
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+games = {}
+
+def create_board():
+    board = np.zeros((ROW_COUNT,COLUMN_COUNT))
+    return board.tolist()
+
+def get_joincode():
+    return ''.join([ str(randint(0, 9)) for _ in range(0, 6)])
+
+app = FastAPI()
+
+@app.get('/create')
+async def create():
+    global games
+    code = get_joincode()
+    game_obj = {
+        "gameover": False,
+        "players": 1,
+        "winner": 0,
+        "turn": 0,
+        "board": create_board()
+    }
+    games[code] = game_obj
+
+    return {"join_code": code, "playerid": 1}
+
+@app.get('/{code}/join')
+async def join(code: str):
+    global games
+
+    game_obj = games.get(code)
+    if not game_obj:
+        return {"status": "FAILED"} 
+
+    if game_obj.get("gameover"):
+        game_obj["gameover"] = False
+        game_obj["players"] = 0
+        game_obj["winner"] = 0
+        game_obj["turn"] = 0
+        game_obj["board"] = create_board()
+
+    game_obj["players"] += 1
+
+    if game_obj.get("players") >= 2:
+        game_obj["players"] = 2
+        game_obj["turn"] = 1
+
+    return {"playerid": game_obj.get("players")}
+
+@app.get('/{code}/sync')
+async def sync(code: str):
+    global games
+    game_obj = games.get(code)
+    if game_obj:
+        return game_obj
+    else:
+        return {"status": "FAILED"}
+
+@app.post('/{code}/update')
+async def update(code: str, request: Request):
+    global games
+    
+    game_obj = games.get(code)
+    if not game_obj:
+        return {"status": "FAILED"} 
+
+    jbody = await request.json()
+    playerid = jbody.get('playerid')
+    game_obj["board"] = jbody.get('board')
+
+    if playerid == 1:
+        game_obj["turn"] = 2
+    elif playerid == 2:
+        game_obj["turn"] = 1
+
+    return {"status": "OK"}
+
+@app.post('/{code}/gameover')
+async def win(code: str, request: Request):
+    global games
+
+    game_obj = games.get(code)
+    if not game_obj:
+        return {"status": "FAILED"} 
+
+    jbody = await request.json()
+    winner = jbody.get('winner')
+
+    game_obj["winner"] = winner
+    game_obj["gameover"] = True
+
+    return {"status": "OK"}
